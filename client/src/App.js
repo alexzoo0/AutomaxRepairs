0aebf59b505ca6f2cd5a2fd7ef521b64442a23df
Alex <alexzoo@Alexzoos-MacBook-Pro.local>
2025-01-11 22:28:06 -0600

    added modified home

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Home from './componets/home/home.js';
 import OurShop from './componets/ourShop/ourShop.js';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
+    {<Home />}
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route  path='/OurShop' element={<OurShop />} />
